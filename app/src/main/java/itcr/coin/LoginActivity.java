0bfded87c75751909e6ee3d5aa94c0a1e1a030e6
Unknown <faph.2009@hotmail.com>
2018-09-15 19:32:02 -0600

    Prueba push/pull git

--- app/src/main/java/itcr/coin/LoginActivity.java
+++ app/src/main/java/itcr/coin/LoginActivity.java
@@ -8,6 +8,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Prueba para github
         setContentView(R.layout.activity_login);
     }
 }
