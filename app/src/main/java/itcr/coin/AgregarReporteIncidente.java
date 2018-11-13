f1080b6549ff8ebe0e829488a1e8b8ee687e3caf
Unknown <aaronypepi@gmail.com>
2018-11-12 23:52:02 -0600

    Layout agregar reporte

---
+++ app/src/main/java/itcr/coin/AgregarReporteIncidente.java
@@ -0,0 +1,13 @@
+package itcr.coin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarReporteIncidente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_reporte_incidente);
+    }
+}
